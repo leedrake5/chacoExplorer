8b6822da6cf77ed8d5e09b7779280f63582d3f91
Lee Drake <b.lee.drake@gmail.com>
2025-04-26 10:36:13 -0600

    Minor fix

--- global.r
+++ global.r
@@ -13,6 +13,9 @@ lm_eqn = function(m) {
     as.character(as.expression(eq));
 }
 
+j = 0.0005
+
+
 data <- read.csv("data/CompiledData-Table 1.csv")
 #colnames(data)[23] <- "latitude"
 #colnames(data)[22] <- "longitude"
@@ -51,18 +54,23 @@ utmcoor<- st_as_sf(data_convert_12, coords = c("Easting", "Northing"), crs = pas
 # converting
 longlatcoor <- st_transform(utmcoor, crs = 4326)  # EPSG:4326 is the WGS84 coordinate system
 coords <- st_coordinates(longlatcoor)
-data_convert_12$Longitude <- round(coords[,1], 3)
-data_convert_12$Latitude <- round(coords[,2], 3)
+data_convert_12$Longitude <- coords[,1]
+data_convert_12$Latitude <- coords[,2]
 
 utmcoor<- st_as_sf(data_convert_13, coords = c("Easting", "Northing"), crs = paste0("+proj=utm +zone=13"))
 #zone= UTM zone
 # converting
 longlatcoor <- st_transform(utmcoor, crs = 4326)  # EPSG:4326 is the WGS84 coordinate system
 coords <- st_coordinates(longlatcoor)
-data_convert_13$Longitude <- round(coords[,1], 3)
-data_convert_13$Latitude <- round(coords[,2], 3)
+data_convert_13$Longitude <- coords[,1]
+data_convert_13$Latitude <- coords[,2]
 
 data <- as.data.frame(data.table::rbindlist(list(data, data_convert_12, data_convert_13), fill=T, use.names=T))
 
+data <- data %>%
+  mutate(
+    Longitude = Longitude + runif(n = n(), min = -j, max = j),
+    Latitude  = Latitude  + runif(n = n(), min = -j, max = j)
+  )
 
 
